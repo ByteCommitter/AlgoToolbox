3fdac99f5d79e401461e94cf4da82df0b0bfccf7
Bangc <vgurumurthy24@gmail.com>
2024-07-01 11:23:07 +0530

    LevelOrderTraversal

--- C++/Trees/LevelOrderTraversal.cpp
+++ C++/Trees/LevelOrderTraversal.cpp
@@ -3,6 +3,17 @@
 #include<vector>
 using namespace std;
 
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(NULL), right(NULL){}
+}
+
+vector<vector<int>> levelOrderTraversal(TreeNode*){
+
+}
+
 int main(){
     
 }
