75b4a086b2311f95cc8f2547954b050962c950e0
Bangc <vgurumurthy24@gmail.com>
2024-07-13 12:03:51 +0530

    DP Start

---
+++ Recursion/Revisiting Recursion/recursion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+createSubsequences(vector<int> v){
+}
+
+
+int main(){
+    int n;
+    vector<int> v={1,4,2,6,7,9,0};
+    //we need to create all subSequences from the given vector. and return it as a vector of vectoes.
+    createSubsequences(v);
+}
